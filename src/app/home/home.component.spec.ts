c8db0b47cfacea4aafe21c1d938211e5d6d58d49
josejbocanegra <josejbocanegra@gmail.com>
2020-03-13 12:22:39 -0500

    First commit

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from "@angular/platform-browser";
+
+import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+  let debug: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [HomeComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    debug = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  //Determinar el valor de una variable en el componente
+  it("Should have a message", () => {
+    expect(component.message).toContain("Users list")
+  })
+
+  //Determinar la existencia de una etiqueta con un valor específico
+  it("Should have an h1 tag", () => {
+    expect(debug.query(By.css("h1")).nativeElement.innerText).toBe("Home component");
+  })
+});
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  message: String = "Users list";
+
+  data: any[];
+
+  constructor(private homeService: HomeService) { }
+
+  ngOnInit() {
+    this.homeService.getData().subscribe(data => {
+      this.data = data;
+    })
+  }
+
+}
